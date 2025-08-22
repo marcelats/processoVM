5799b70aa94080dd66177ddc5f5e586b3d02d91d
marcelats <marcelatshinzato@gmail.com>
2025-08-22 11:57:39 -0300

    Update app.py

--- app.py
+++ app.py
@@ -60,17 +60,17 @@ async def execute(code: UploadFile = File(...), lang: str = Form(...)):
                 "python:3.11-slim",
                 command=["python", "/workspace/code.py"],  # note o path no container
                 volumes={tmpdir: {"bind": "/workspace", "mode": "rw"}},
-                detach=True,
+                detach=False,
                 auto_remove=False
             )
-            container.reload()
+       
             # Agora você pode executar comandos dentro do container
             exit_code, output = container.exec_run("ls -l /workspace")
             print(output.decode())
-            container.reload()
+   
             exit_code, output = container.exec_run(f"cat {container_file_path}")
             print(output.decode())
-            container.reload()
+  
             # Depois finalize o container
             logs = container.logs().decode("utf-8")
             container.stop()
