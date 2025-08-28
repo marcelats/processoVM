73e11307b78cbf068eac30ee4b0153333716b53a
marcelats <marcelatshinzato@gmail.com>
2025-08-28 12:46:31 -0300

    Update Dockerfile.r

--- Dockerfile.r
+++ Dockerfile.r
@@ -1,4 +1,4 @@
-FROM rocker/r-ver:4.3.1  # ou a versão que você precisar
+FROM rocker/r-ver:4.3.1 
 WORKDIR /workspace
 RUN R -e "install.packages('simmer', repos='https://cloud.r-project.org')"
 CMD ["Rscript"]
